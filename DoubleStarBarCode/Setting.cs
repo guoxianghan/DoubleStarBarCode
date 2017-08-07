dddb23e89778b08061376a0088d239ba863e3f5a
guoxianghan <1146267220@qq.com>
2017-08-07 16:25:59 +0800

    初始化双星扫码项目,串口通信初始化

---
+++ DoubleStarBarCode/Setting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoubleStarBarCode
+{
+    public class Setting
+    {
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public string COM { get; set; }
+        /// <summary>
+        /// 字符串or16进制
+        /// </summary>
+        public string StringOrHex { get; set; }
+        /// <summary>
+        /// 波特率
+        /// </summary>
+        public string baudRate { get; set; }
+        /// <summary>
+        /// 数据位
+        /// </summary>
+        public string dataBits { get; set; }
+        /// <summary>
+        /// 停止位
+        /// </summary>
+        public string stopBits { get; set; }
+        /// <summary>
+        /// 校验位
+        /// </summary>
+        public string parity { get; set; }
+
+        public string handshaking { get; set; }
+    }
+}
---
+++ DoubleStarBarCode/frmSetting.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace DoubleStarBarCode
+{
+    partial class frmSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.handshakingcbx = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.refreshbtn = new System.Windows.Forms.Button();
+            this.dataBitsCbx = new System.Windows.Forms.ComboBox();
+            this.comListCbx = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openCloseSpbtn = new System.Windows.Forms.Button();
+            this.baudRateCbx = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.parityCbx = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.stopBitsCbx = new System.Windows.Forms.ComboBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.handshakingcbx);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.refreshbtn);
+            this.groupBox2.Controls.Add(this.dataBitsCbx);
+            this.groupBox2.Controls.Add(this.comListCbx);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.openCloseSpbtn);
+            this.groupBox2.Controls.Add(this.baudRateCbx);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.parityCbx);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.stopBitsCbx);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(103, 355);
+            this.groupBox2.TabIndex = 25;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "COM";
+            // 
+            // handshakingcbx
+            // 
+            this.handshakingcbx.FormattingEnabled = true;
+            this.handshakingcbx.Location = new System.Drawing.Point(14, 235);
+            this.handshakingcbx.Name = "handshakingcbx";
+            this.handshakingcbx.Size = new System.Drawing.Size(74, 20);
+            this.handshakingcbx.TabIndex = 24;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 220);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 12);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "HandShaking:";
+            // 
+            // refreshbtn
+            // 
+            this.refreshbtn.Location = new System.Drawing.Point(14, 267);
+            this.refreshbtn.Name = "refreshbtn";
+            this.refreshbtn.Size = new System.Drawing.Size(74, 32);
+            this.refreshbtn.TabIndex = 22;
+            this.refreshbtn.Text = "刷新";
+            this.refreshbtn.UseVisualStyleBackColor = true;
+            // 
+            // dataBitsCbx
+            // 
+            this.dataBitsCbx.FormattingEnabled = true;
+            this.dataBitsCbx.Location = new System.Drawing.Point(14, 115);
+            this.dataBitsCbx.Name = "dataBitsCbx";
+            this.dataBitsCbx.Size = new System.Drawing.Size(74, 20);
+            this.dataBitsCbx.TabIndex = 11;
+            // 
+            // comListCbx
+            // 
+            this.comListCbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comListCbx.FormattingEnabled = true;
+            this.comListCbx.Location = new System.Drawing.Point(14, 35);
+            this.comListCbx.Name = "comListCbx";
+            this.comListCbx.Size = new System.Drawing.Size(74, 20);
+            this.comListCbx.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "端口:";
+            // 
+            // openCloseSpbtn
+            // 
+            this.openCloseSpbtn.Enabled = false;
+            this.openCloseSpbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.openCloseSpbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openCloseSpbtn.Location = new System.Drawing.Point(14, 311);
+            this.openCloseSpbtn.Name = "openCloseSpbtn";
+            this.openCloseSpbtn.Size = new System.Drawing.Size(74, 36);
+            this.openCloseSpbtn.TabIndex = 17;
+            this.openCloseSpbtn.Text = "Open";
+            this.openCloseSpbtn.UseVisualStyleBackColor = true;
+            // 
+            // baudRateCbx
+            // 
+            this.baudRateCbx.FormattingEnabled = true;
+            this.baudRateCbx.Location = new System.Drawing.Point(14, 75);
+            this.baudRateCbx.Name = "baudRateCbx";
+            this.baudRateCbx.Size = new System.Drawing.Size(74, 20);
+            this.baudRateCbx.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "校验位:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "波特率:";
+            // 
+            // parityCbx
+            // 
+            this.parityCbx.FormattingEnabled = true;
+            this.parityCbx.Location = new System.Drawing.Point(14, 195);
+            this.parityCbx.Name = "parityCbx";
+            this.parityCbx.Size = new System.Drawing.Size(74, 20);
+            this.parityCbx.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "数据位:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "停止位:";
+            // 
+            // stopBitsCbx
+            // 
+            this.stopBitsCbx.FormattingEnabled = true;
+            this.stopBitsCbx.Location = new System.Drawing.Point(14, 155);
+            this.stopBitsCbx.Name = "stopBitsCbx";
+            this.stopBitsCbx.Size = new System.Drawing.Size(74, 20);
+            this.stopBitsCbx.TabIndex = 13;
+            // 
+            // frmSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 445);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "frmSetting";
+            this.Text = "设置";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox handshakingcbx;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button refreshbtn;
+        private System.Windows.Forms.ComboBox dataBitsCbx;
+        private System.Windows.Forms.ComboBox comListCbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button openCloseSpbtn;
+        private System.Windows.Forms.ComboBox baudRateCbx;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox parityCbx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox stopBitsCbx;
+    }
+}
